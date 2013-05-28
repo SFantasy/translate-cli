40079d84d0e63f66a45967020c6031d16e64bec7
SFantasy <forever.fantasy27@gmail.com>
2013-05-28 19:54:08 +0800

    updated dic.sh

--- dic.sh
+++ dic.sh
@@ -2,4 +2,4 @@
 
 result=$(curl 'http://openapi.baidu.com/public/2.0/bmt/translate?client_id=IGRde9G9hsF3y6zr4aVPvMxq&from=auto&to=auto&q='+$1 --silent | ./JSON.sh -l | awk '/\["trans_result",0,"dst"]/ {print $2 }')
 
-echo -e $result
+echo -e $result | tr -d '"'
