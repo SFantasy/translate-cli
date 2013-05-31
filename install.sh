c5f0354e41e5e80d2df5012f06290de3743a6419
SFantasy <forever.fantasy27@gmail.com>
2013-05-31 21:27:31 +0800

    Updated install.sh and fix some bugs

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Installing...(this may need your root password)"
-su -
-chmod 775 JSON.sh
-ln ./JSON.sh /bin/JSON
-chmod 775 dic.sh
-ln ./dic.sh /bin/dic
+sudo chmod 775 JSON.sh
+sudo ln ./JSON.sh /bin/JSON
+sudo chmod 775 dic.sh
+sudo ln ./dic.sh /bin/dic
 echo "translate-cli installed."
