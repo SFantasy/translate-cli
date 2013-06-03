93ce9af8add7f596b06c9772ecf2cff8b7e220e7
SFantasy <forever.fantasy27@gmail.com>
2013-06-03 19:53:51 +0800

    Specified translate languages

--- dic.sh
+++ dic.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-result=$(curl 'http://openapi.baidu.com/public/2.0/bmt/translate?client_id=IGRde9G9hsF3y6zr4aVPvMxq&from=auto&to=auto&q='+$1 --silent | JSON -l | awk -F "\t" '/\["trans_result",0,"dst"]/ {print $2 }')
+result=$(curl 'http://openapi.baidu.com/public/2.0/bmt/translate?client_id=IGRde9G9hsF3y6zr4aVPvMxq&from=en&to=zh&q='+$1 --silent | JSON -l | awk -F "\t" '/\["trans_result",0,"dst"]/ {print $2 }')
 
 echo -e $result | tr -d '"'
