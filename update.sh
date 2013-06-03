76eedcf8caf47fedd3289c21cee81708c82b2869
SFantasy <forever.fantasy27@gmail.com>
2013-06-03 19:58:18 +0800

    Updated update.sh

--- update.sh
+++ update.sh
@@ -5,8 +5,11 @@
 echo 'Update begin...'
 echo 'There may be some warning that you donnot have to care about.'
 
-wget https://raw.github.com/SFantasy/translate-cli/master/dic.sh
+wget https://raw.github.com/SFantasy/translate-cli/master/dic.sh dic.sh
+wget https://raw.github.com/SFantasy/translate-cli/master/trans.sh trans.sh
 sudo rm /bin/dic
 sudo ln ./dic.sh /bin/dic
+sudo rm /bin/trans
+sudo ln ./trans.sh /bin/trans
 
 echo 'Updated.'
