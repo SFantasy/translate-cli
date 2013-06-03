9dcc5d76822365714bc1687d8b58dfc062301390
SFantasy <forever.fantasy27@gmail.com>
2013-06-03 19:57:12 +0800

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+#Filename: install.sh
+#Author: forever.fantasy27@gmail.com
 
 echo "Installing...(this may need your root password)"
 sudo chmod 775 JSON.sh
 sudo ln ./JSON.sh /bin/JSON
+sudo chmod 775 trans.sh
+sudo ln ./dic.sh /bin/trans
 sudo chmod 775 dic.sh
 sudo ln ./dic.sh /bin/dic
 echo "translate-cli installed."
